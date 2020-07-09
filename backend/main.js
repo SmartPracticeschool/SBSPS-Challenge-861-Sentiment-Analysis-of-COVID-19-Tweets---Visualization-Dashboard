8a8db26d8ecccb640ecfa319040bdaf9e7d94022
rahul-hebbar <rhin1998@gmail.com>
2020-07-09 22:25:31 +0530

    line chart changes

--- backend/main.js
+++ backend/main.js
@@ -11,35 +11,92 @@ const io = require("socket.io")(server);
 const sqlite3 = require("sqlite3").verbose();
 let db = new sqlite3.Database("./twitter.db");
 
-const sendPie = () => {
+const allData = () => {
   let sql = "SELECT * FROM sentiment";
-  var obj = [
-    { id: "positive", label: "positive", value: 0 },
-    { id: "negative", label: "negative", value: 0 },
-    { id: "neutral", label: "neutral", value: 0 },
+  let list = 
+  [{
+      id: 'positive',
+      color: 'hsl(198, 70%, 50%)',
+      data: []
+   },
+   {
+       id:'negative',
+       color: 'hsl(20, 70%, 50%)',
+       data: []
+   },
+   {
+       id:'neutral',
+       color:'hsl(90, 70%, 50%)',
+       data: []
+   }
   ];
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    rows.forEach((row) => {
-      var sentiment = row.sentiment;
-      if (sentiment >= 0.2) {
-        obj[0].value++;
-      } else if (sentiment <= -0.2) {
-        obj[1].value++;
-      } else {
-        obj[2].value++;
-      }
-    });
-    io.emit("pie", obj);
-    console.log("pie data: ", obj);
+  var pie = [
+      { id: "positive", label: "positive", value: 0 },
+      { id: "negative", label: "negative", value: 0 },
+      { id: "neutral", label: "neutral", value: 0 },
+    ];
+  db.all(sql, [], (err,rows) =>{
+      var flag = null;
+      var val = {pos:0,neg:0,neu:0};
+      if (err){
+          throw err;
+      };
+      rows.forEach((row) =>{
+          var sentiment = row.sentiment;
+          var date = row.created_at.slice(26,)+row.created_at.slice(3,16);
+          if (date === flag ){
+              // console.log(date,flag)
+              if (sentiment >= 0.2){
+                  pie[0].value++;
+                  val.pos++;
+              } else if (sentiment <= -0.2){
+                  pie[1].value++;
+                  val.neg++;
+              } else {
+                  pie[2].value++;
+                  val.neu++;
+              }
+          }
+          else{
+              // console.log(date,flag)
+              if (flag !== null){
+                  // console.log('not null')
+                  list[0].data.push({ x:flag, y:val.pos });
+                  list[1].data.push({ x:flag, y:val.neg });
+                  list[2].data.push({ x:flag, y:val.neu });
+              }
+              flag = date;
+              if (sentiment >= 0.2) {
+                  pie[0].value++;
+                  val.pos = 1;
+                  val.neg = 0;
+                  val.neu = 0;
+              } else if (sentiment <= -0.2) {
+                  pie[1].value++;
+                  val.pos = 0;
+                  val.neg = 1;
+                  val.neu = 0;
+              } else {
+                  pie[2].value++;
+                  val.pos = 0;
+                  val.neg = 0;
+                  val.neu = 1;
+              }
+          }
+      })
+      list[0].data.push({ x:flag, y:val.pos });
+      list[1].data.push({ x:flag, y:val.neg });
+      list[2].data.push({ x:flag, y:val.neu });
+      console.log(JSON.stringify(list,null,4))
+      console.log(pie)
+      io.emit("pie", pie);
+      io.emit("line", list);
   });
-};
+}
 
 io.on("connection", (socket) => {
   console.log("client connected");
   socket.on("dashboard", () => {
-    sendPie();
+    allData();
   });
 });
