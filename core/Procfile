6d8fae968be622ee1bdd3afb269bb621ef19779a
Nipun Haldar <nipunhaldar24@gmail.com>
2020-07-13 15:27:16 +0530

    Created template for python deployment

---
+++ core/Procfile
@@ -0,0 +1,1 @@
+web: python ml_server.py
--- core/ml_server.py
+++ core/ml_server.py
@@ -16,4 +16,4 @@ def analyse():
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
